c4e1bfe4fcd4ac1e5c4b09737e9e448ee700cf11
AhoySmallFry <jordana@makin.co>
2021-09-10 17:04:19 +0100

    added sunset time

--- src/javascript.js
+++ src/javascript.js
@@ -99,6 +99,14 @@ function showWeather(response) {
 
   let humidityElement = document.querySelector("#precipitation");
   humidityElement.innerHTML = `${response.data.main.humidity}%`;
+
+  let sunsetElement = document.querySelector("#sunsetTime");
+  let unix_timestamp = response.data.sys.sunset;
+  let date = new Date(unix_timestamp * 1000);
+  let hours = date.getHours();
+  let minutes = "0" + date.getMinutes();
+  let formattedTime = hours + ":" + minutes.substr(-2);
+  sunsetElement.innerHTML = `${formattedTime}`;
 }
 
 function retrievePosition(position) {
